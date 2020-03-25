f53adfb4ccfc88b617d7413bfa336fcae3a8ccd8
Cor Zuurmond <jczuurmond@protonmail.com>
2020-03-25 14:14:31 +0100

    Add my_computation

--- loader.py
+++ loader.py
@@ -1,2 +1,11 @@
+import time
+
+
 def create_data():
-    return [1, 39, 2, 4]
+    return [1, 39, 2, 4, 5, 9, 10]
+
+
+def my_computation():
+    # Very expensive computation
+    time.sleep(5)
+    print('hello')
